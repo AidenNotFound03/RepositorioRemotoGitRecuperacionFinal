43253c29f897b67d9e465bb711889f190a7be9a3
Daw <Daw@DESKTOP-8OVFM6T>
2023-06-19 17:52:54 +0200

    cambios

---
+++ src/paqueteGitRecuperacionFinal/Mensaje.java
@@ -0,0 +1,10 @@
+package paqueteGitRecuperacionFinal;
+
+public class Mensaje {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mensaje de prueba");
+	}
+
+}
